2c76b6bc2d34a72a057b81f7793cc4770b0835ee
Bruce Flynn <brucef@ssec.wisc.edu>
2025-10-27 10:17:08 -0500

    Add deadline support

--- conn.go
+++ conn.go
@@ -1,24 +1,16 @@
 package telnet
 
-
 import (
 	"crypto/tls"
 	"net"
+	"time"
 )
 
-
 type Conn struct {
-	conn interface {
-		Read(b []byte) (n int, err error)
-		Write(b []byte) (n int, err error)
-		Close() error
-		LocalAddr() net.Addr
-		RemoteAddr() net.Addr
-	}
+	conn       net.Conn
 	dataReader *internalDataReader
 	dataWriter *internalDataWriter
 }
-
 
 // Dial makes a (un-secure) TELNET client connection to the system's 'loopback address'
 // (also known as "localhost" or 127.0.0.1).
@@ -33,7 +25,6 @@ func Dial() (*Conn, error) {
 //
 // If a secure connection is desired, use `DialToTLS` instead.
 func DialTo(addr string) (*Conn, error) {
-
 	const network = "tcp"
 
 	if "" == addr {
@@ -49,14 +40,13 @@ func DialTo(addr string) (*Conn, error) {
 	dataWriter := newDataWriter(conn)
 
 	clientConn := Conn{
-		conn:conn,
-		dataReader:dataReader,
-		dataWriter:dataWriter,
+		conn:       conn,
+		dataReader: dataReader,
+		dataWriter: dataWriter,
 	}
 
 	return &clientConn, nil
 }
-
 
 // DialTLS makes a (secure) TELNETS client connection to the system's 'loopback address'
 // (also known as "localhost" or 127.0.0.1).
@@ -83,15 +73,13 @@ func DialToTLS(addr string, tlsConfig *tls.Config) (*Conn, error) {
 	dataWriter := newDataWriter(conn)
 
 	clientConn := Conn{
-		conn:conn,
-		dataReader:dataReader,
-		dataWriter:dataWriter,
+		conn:       conn,
+		dataReader: dataReader,
+		dataWriter: dataWriter,
 	}
 
 	return &clientConn, nil
 }
-
-
 
 // Close closes the client connection.
 //
@@ -107,7 +95,6 @@ func (clientConn *Conn) Close() error {
 	return clientConn.conn.Close()
 }
 
-
 // Read receives `n` bytes sent from the server to the client,
 // and "returns" into `p`.
 //
@@ -122,7 +109,6 @@ func (clientConn *Conn) Read(p []byte) (n int, err error) {
 	return clientConn.dataReader.Read(p)
 }
 
-
 // Write sends `n` bytes from 'p' to the server.
 //
 // Note that Write can only be used for sending TELNET (and TELNETS) data to the server.
@@ -135,14 +121,27 @@ func (clientConn *Conn) Write(p []byte) (n int, err error) {
 	return clientConn.dataWriter.Write(p)
 }
 
-
 // LocalAddr returns the local network address.
 func (clientConn *Conn) LocalAddr() net.Addr {
 	return clientConn.conn.LocalAddr()
 }
 
-
 // RemoteAddr returns the remote network address.
 func (clientConn *Conn) RemoteAddr() net.Addr {
 	return clientConn.conn.RemoteAddr()
+}
+
+// SetReadDeadline sets the deadline for future Read calls.
+func (clientConn *Conn) SetDeadline(t time.Time) error {
+	return clientConn.conn.SetReadDeadline(t)
+}
+
+// SetWriteDeadline sets the deadline for future Write calls.
+func (clientConn *Conn) SetReadDeadline(t time.Time) error {
+	return clientConn.conn.SetReadDeadline(t)
+}
+
+// SetWriteDeadline sets the deadline for future Write calls.
+func (clientConn *Conn) SetWriteDeadline(t time.Time) error {
+	return clientConn.conn.SetReadDeadline(t)
 }
